0a9c464b8738b1393e64a53e8a7131a5248f58dd
Sebastian <sebasrojasm1@gmail.com>
2024-09-21 15:02:08 -0500

    Is fixed routes in the app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ProductModule } from './module/product/product.module';
-import { CategoryModule } from './module/category/category.module';
-import { OrderModule } from './module/order/order.module';
-import { OrderProductModule } from './module/order-product/order-product.module';
-import { PaymentModule } from './module/payment/payment.module';
-import { ReviewModule } from './module/review/review.module';
-import { AddressModule } from './module/address/address.module';
-import { InventoryModule } from './module/inventory/inventory.module';
+import { AddressController } from './module/address/controllers/address.controller';
+import { CategoryController } from './module/category/controllers/category.controller';
+import { InventoryController } from './module/inventory/controllers/inventory.controller';
+import { OrderController } from './module/order/controllers/order.controller';
+import { OrderProductController } from './module/order-product/controllers/order-product.controller';
+import { PaymentController } from './module/payment/controllers/payment.controller';
+import { ProductController } from './module/product/controllers/product.controller';
+import { ReviewController } from './module/review/controllers/review.controller';
+import { UserController } from './module/user/controllers/user.controller';
+import { AddressService } from './module/address/services/address.service';
+import { CategoryService } from './module/category/services/category.service';
+import { InventoryService } from './module/inventory/services/inventory.service';
+import { OrderService } from './module/order/services/order.service';
+import { OrderProductService } from './module/order-product/services/order-product.service';
+import { PaymentService } from './module/payment/services/payment.service';
+import { ProductService } from './module/product/services/product.service';
+import { ReviewService } from './module/review/services/review.service';
+import { UserService } from './module/user/services/user.service';
 
 
 @Module({
-  imports: [ProductModule, CategoryModule, OrderModule, OrderProductModule, PaymentModule, ReviewModule, AddressModule, InventoryModule],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AddressController, CategoryController, InventoryController, OrderController, OrderProductController, PaymentController, ProductController, ReviewController, UserController],
+  providers: [AddressService, CategoryService, InventoryService, OrderService, OrderProductService, PaymentService, ProductService, ReviewService, UserService],
 })
 export class AppModule {}
