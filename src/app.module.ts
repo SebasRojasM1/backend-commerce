3e447c4644079365621e8e2f84a88c0838330f7c
Sebastian <sebasrojasm1@gmail.com>
2024-09-21 14:25:50 -0500

    The Payment Entity was created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ProductModule } from './module/product/product.module';
 import { CategoryModule } from './module/category/category.module';
 import { OrderModule } from './module/order/order.module';
 import { OrderProductModule } from './module/order-product/order-product.module';
+import { PaymentModule } from './module/payment/payment.module';
 
 
 @Module({
-  imports: [ProductModule, CategoryModule, OrderModule, OrderProductModule],
+  imports: [ProductModule, CategoryModule, OrderModule, OrderProductModule, PaymentModule],
   controllers: [],
   providers: [],
 })
