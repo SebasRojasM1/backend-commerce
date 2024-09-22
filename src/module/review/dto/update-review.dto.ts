2c88db61bdd4a6bd582cf096eddfcd539d2ccb6d
Sebastian <sebasrojasm1@gmail.com>
2024-09-22 16:48:47 -0500

    The review DTO was created

--- src/module/review/dto/update-review.dto.ts
+++ src/module/review/dto/update-review.dto.ts
@@ -1,4 +1,24 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateReviewDto } from './create-review.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
-export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
+export class UpdateReviewDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  userId: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  productId: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  rating: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  comment: string;
+}
