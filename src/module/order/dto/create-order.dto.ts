3d6a1ff92adb7d478179130bac3dee60ca797523
Sebastian <sebasrojasm1@gmail.com>
2024-09-22 16:42:31 -0500

    The order DTO was created

--- src/module/order/dto/create-order.dto.ts
+++ src/module/order/dto/create-order.dto.ts
@@ -1,1 +1,19 @@
-export class CreateOrderDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreateOrderDto {
+    @ApiProperty()
+    @IsNotEmpty()
+  @IsNumber()
+  userId: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  totalAmount: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEnum(['pending', 'shipped', 'delivered', 'canceled'])
+  status: 'pending' | 'shipped' | 'delivered' | 'canceled';
+}
